190a7feb3e1e12646491e0de8c5f9de38a93e10c
Aisalkyn <aisalkyn.4160@gmail.com>
2021-11-14 14:09:42 +0600

    set static and media urls and roots

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
  
 def homepage(request):
-     return HttpResponse('hello world!')
+    return render(request, "index.html")
 
 
 def test(request):
