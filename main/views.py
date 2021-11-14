2a119e43b3b1eb358b52692b737667c6ce02bdea
Aisalkyn <aisalkyn.4160@gmail.com>
2021-11-14 22:17:29 +0600

    added unmark and is_closed todo

--- main/views.py
+++ main/views.py
@@ -26,6 +26,13 @@ def delete_todo(request, id):
 
 def mark_todo(request, id):
     todo = ToDo.objects.get(id=id)
-    todo.is_favorite = True
+    todo.is_favorite = not todo.is_favorite
+    todo.save()
+    return redirect(test)
+
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
     todo.save()
     return redirect(test)
