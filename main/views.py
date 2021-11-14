50da1ebcbb0f4f8de4de9e15e9e403d00f708f1f
Aisalkyn <aisalkyn.4160@gmail.com>
2021-11-14 13:07:57 +0600

    init project

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
+ 
+def homepage(request):
+     return HttpResponse('hello world!')
+
+
+def test(request):
+    return render(request, 'test.html')
